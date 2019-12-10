803f26b887da9c3257ac9a788493fbc54a6b50d7
Martin <martinjonsson01@gmail.com>
2019-12-10 14:08:15 +0100

    Lagt till routing och arbetat på authentication

--- src/components/App/PageContainer.jsx
+++ src/components/App/PageContainer.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components';
+import { Route } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
 import TheoryPage from '../TheoryPage/TheoryPage';
 import multiplicationImage from '../../media/multiplication.jpg'
-
 
 function PageContainer(props) {
   return (
     <Layout>
-    <TheoryPage headerImage = {multiplicationImage} subject="Multiplikation">
-
-    </TheoryPage>
+      <Route
+        path={ROUTES.SCOREBOARD}
+        render={(props) => "Scoreboard"}
+      />
+      <Route
+        path={ROUTES.HOME}
+        render={(props) => <TheoryPage {...props} headerImage={multiplicationImage} subject="Multiplikation" />}
+      />
+      <Route
+        path={ROUTES.ACCOUNT}
+        render={(props) => "Account"}
+      />
     </Layout>
   )
 }
 
 PageContainer.propTypes = {
-  
+
 }
 
 const Layout = styled.div`
   height: calc(100vh - 5rem);
   background: #fafafa;
-  overflow:scroll;
+  overflow: auto;
 `;
 
 export default PageContainer
--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-import WebFont from 'webfontloader'
+import WebFont from 'webfontloader';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 WebFont.load(
     {
         google:
         {
-            families:["Nunito"]
+            families: ["Nunito"]
         }
     });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
