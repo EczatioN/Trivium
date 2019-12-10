a1d58cdd75494edd4fa109d76491363a21276898
Martin <martinjonsson01@gmail.com>
2019-12-10 11:18:57 +0100

    Arbetat på login och navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
+//import styled from 'styled-components';
+//import { useAuthState } from 'react-firebase-hooks/auth';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import PageContainer from './PageContainer';
 import Navbar from './Navbar';
 
 function App() {
+  //console.log(firebase);
+  //const [user, loading, error] = useAuthState(window.firebase.auth.Auth);
   return (
-    <Layout>
-      <PageContainer />
-      <Navbar />
-    </Layout>
+    <Router>
+      {true &&
+        <React.Fragment>
+          <PageContainer />
+          <Navbar />
+        </React.Fragment>
+      }
+    </Router>
   );
 }
-
-const Layout = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
 
 export default App;
