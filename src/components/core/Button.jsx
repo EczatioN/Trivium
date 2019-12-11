49db37b0ef2f9f48a68a953ca003ae7f18bc33dc
arexonetwo <alexandar.cvetkovic@elev.ga.lbs.se>
2019-12-11 10:31:34 +0100

    Theory Page nästan klar, även gjort Komponent för knapp.

--- src/components/core/Button.jsx
+++ src/components/core/Button.jsx
@@ -3,27 +3,50 @@ import PropTypes from 'prop-types'
 import MaterialIcon from "@material/react-material-icon"
 import styled from "styled-components"
 
-function Button({icon, text, backgroundColor}) {
+function Button({icon, text, backgroundColor, backgroundColorAfter, className}) {
   return (
-    <Layout backgroundColor = {backgroundColor}>
-      {icon &&
-        <MaterialIcon icon={icon}></MaterialIcon>
-      }
+    <Layout
+      className = {className}
+      backgroundColor = {backgroundColor}
+      backgroundColorAfter = {backgroundColorAfter}
+      >
       {text &&
-        text
+        <span>{text}</span>
+      }
+
+      {icon &&
+        <StyledMaterialIcon icon={icon}></StyledMaterialIcon>
       }
     </Layout>)
 }
 
 const Layout = styled.div`
 background: ${props => props.backgroundColor};
+border-radius: 0.5rem;
+transition: all 0.05s ease-in-out;
+box-shadow: 5px 5px 6px 1px rgba(0, 0, 0, .4);
+&:active {
+  box-shadow 6px 6px 6px 4px rgba(0, 0, 0, .4);
+  transform: scale(1.025);
+  background: ${props => props.backgroundColorAfter};
+}
+span {
+  user-select:none;
+}
 
+`;
+
+const StyledMaterialIcon = styled(MaterialIcon)`
+  font-size: 4rem;
+  color:#f3f9fe ;
+  user-select: none;
 `;
 
 Button.propTypes = {
   icon: PropTypes.string,
   text: PropTypes.string,
   backgroundColor: PropTypes.string,
+  className: PropTypes.any,
 
 }
 
