4a003ed1c9ec8c562fc072f6f9c037446b4628be
Martin <martinjonsson01@gmail.com>
2019-12-12 11:42:49 +0100

    Update Button.jsx

--- src/components/core/Button.jsx
+++ src/components/core/Button.jsx
@@ -3,50 +3,49 @@ import PropTypes from 'prop-types'
 import MaterialIcon from "@material/react-material-icon"
 import styled from "styled-components"
 
-function Button({icon, text, backgroundColor, backgroundColorAfter, className}) {
+function Button({ icon, text, backgroundColor, backgroundColorAfter, className, onClick, iconColor }) {
   return (
     <Layout
-      className = {className}
-      backgroundColor = {backgroundColor}
-      backgroundColorAfter = {backgroundColorAfter}
-      >
+      onClick={onClick}
+      className={className}
+      backgroundColor={backgroundColor}
+      backgroundColorAfter={backgroundColorAfter}
+    >
       {text &&
         <span>{text}</span>
       }
-
       {icon &&
-        <StyledMaterialIcon icon={icon}></StyledMaterialIcon>
+        <StyledMaterialIcon iconcolor={iconColor} icon={icon}></StyledMaterialIcon>
       }
     </Layout>)
 }
 
 const Layout = styled.div`
-background: ${props => props.backgroundColor};
-border-radius: 0.5rem;
-display:flex;
-align-items:center;
-text-align: center;
-margin: auto;
-height: 4rem;
-padding: 0.25rem 1rem;
-font-size:1.5rem;
-color:#f3f9fe;
-transition: all 0.05s ease-in-out;
-box-shadow: 5px 5px 6px 1px rgba(0, 0, 0, .4);
-&:active {
-  box-shadow 6px 6px 6px 4px rgba(0, 0, 0, .4);
-  transform: scale(1.025);
-  background: ${props => props.backgroundColorAfter};
-}
-span {
-  user-select:none;
-}
-
+  background: ${props => props.backgroundColor};
+  border-radius: 0.5rem;
+  display:flex;
+  align-items:center;
+  text-align: center;
+  margin: auto;
+  height: 4rem;
+  padding: 0.25rem 1rem;
+  font-size:1.5rem;
+  color:#f3f9fe;
+  transition: all 0.05s ease-in-out;
+  box-shadow: 5px 5px 6px 1px rgba(0, 0, 0, .4);
+  &:active {
+    box-shadow 6px 6px 6px 4px rgba(0, 0, 0, .4);
+    transform: scale(1.025);
+    background: ${props => props.backgroundColorAfter};
+  }
+  span {
+    user-select:none;
+  }
 `;
 
 const StyledMaterialIcon = styled(MaterialIcon)`
   font-size: 4rem;
-  color:#f3f9fe ;
+  color:${props => props.iconColor ? props.iconcolor : "#f3f9fe"};
   user-select: none;
 `;
 
@@ -55,6 +54,7 @@ Button.propTypes = {
   text: PropTypes.string,
   backgroundColor: PropTypes.string,
   className: PropTypes.any,
+  iconColor: PropTypes.string,
 
 }
 
